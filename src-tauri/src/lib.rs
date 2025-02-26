849ebd0e2165900db9759a14bc489b352ae25b1a
Artur Kovacs <artur.kovacs@codecool.com>
2025-02-26 11:52:14 +0100

    fix crash on startup and add zoom

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,6 @@
 use std::path::PathBuf;
 
-fn get_file_path() -> PathBuf {
+fn get_file_path() -> Option<PathBuf> {
     let mut files = Vec::new();
 
     // NOTICE: `args` may include URL protocol (`your-app-protocol://`)
@@ -9,43 +9,46 @@ fn get_file_path() -> PathBuf {
     for maybe_file in std::env::args().skip(1) {
         // skip flags like -f or --flag
         if maybe_file.starts_with('-') {
-        continue;
+            continue;
         }
 
         // handle `file://` path urls and skip other urls
         if let Ok(url) = tauri::Url::parse(&maybe_file) {
-        if let Ok(path) = url.to_file_path() {
-            files.push(path);
-        }
+            if let Ok(path) = url.to_file_path() {
+                files.push(path);
+            }
         } else {
-        files.push(PathBuf::from(maybe_file))
+            files.push(PathBuf::from(maybe_file))
         }
     }
 
     // TODO: handle opening multiple files
-    files.into_iter().next().unwrap()
+    files.into_iter().next()
 }
 
 mod commands {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
     #[tauri::command]
     pub fn get_file_path() -> String {
-        super::get_file_path().to_str().unwrap().to_owned()
+        super::get_file_path().map(|p| p.to_str().unwrap().to_owned()).unwrap_or_default()
     }
-    
+
     #[tauri::command]
-    pub async fn get_content_html() -> String {
-        let path = super::get_file_path();
+    pub async fn get_content_html() -> Result<String, String> {
+        let path = match super::get_file_path() {
+            Some(p) => p,
+            None => return Err(format!("No file selected."))
+        };
         let markdown = match std::fs::read_to_string(&path) {
             Ok(markdown) => markdown,
             Err(e) => {
-                return format!("Could not read file '{path:?}': {e}");
+                return Err(format!("Could not read file '{path:?}': {e}"));
             }
         };
         let mut html = String::new();
         let parser = pulldown_cmark::Parser::new(&markdown);
         pulldown_cmark::html::push_html(&mut html, parser);
-        html
+        Ok(html)
     }
 }
 
@@ -54,7 +57,8 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
-            commands::get_content_html, commands::get_file_path
+            commands::get_content_html,
+            commands::get_file_path
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,49 @@
-import { createSignal, onMount, Show } from "solid-js";
+import { createSignal, onCleanup, onMount, Show } from "solid-js";
 
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 import "./markdown.css";
 
+function setZoom(level: number) {
+  level = Math.min(3, Math.max(0.3, level));
+  document.body.style.zoom = String(level);
+}
+
+function getZoom(): number {
+  return Number(document.body.style.zoom);
+}
+
+const ZOOM_DELTA = 1.1;
+
 function App() {
   const [filePath, setFilePath] = createSignal<string | null>(null);
   const [contentHtml, setContentHtml] = createSignal<string | null>(null);
+  const [errorMessage, setErrorMessage] = createSignal<string | null>(null);
 
-  // async function greet() {
-  //   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-  //   setGreetMsg(await invoke("greet", { name: name() }));
-  // }
-  
+  const handleZoomKey = (event: KeyboardEvent) => {
+    if (event.ctrlKey) {
+      if (event.key === '=') {
+        setZoom(getZoom() * ZOOM_DELTA);
+      } else if (event.key === "-") {
+        setZoom(getZoom() / ZOOM_DELTA);
+      }
+    }
+  };
+
   onMount(async () => {
+    setZoom(1);
+    document.addEventListener("keydown", handleZoomKey);
+
     setFilePath(await invoke("get_file_path"));
-    setContentHtml(await invoke("get_content_html"));
+    invoke("get_content_html").then(html => {
+      setContentHtml(html as string);
+    }).catch(err => {
+      setErrorMessage(err);
+    });
+  });
+
+  onCleanup(() => {
+    document.removeEventListener("keydown", handleZoomKey);
   });
 
   return (
@@ -24,6 +52,11 @@ function App() {
         {filePath()}
       </div>
 
+      <Show when={errorMessage() != null}>
+        <div>
+          {errorMessage()}
+        </div>
+      </Show>
       <Show when={contentHtml() != null}>
         <div class="markdown-content" innerHTML={contentHtml() ?? undefined}></div>
       </Show>
